f240301f27520eabac2e452fb9aa3162d7c86b54
guilhermegouw <guilherme.gouw@gmail.com>
2024-06-06 16:35:27 -0300

    Implemented format on save

--- lua/minimalist_pythonist/init.lua
+++ lua/minimalist_pythonist/init.lua
@@ -1,93 +1,111 @@
--- Minimalist Pythonist Initialization
+local vim = vim
+local api = vim.api
+local fn = vim.fn
+local uv = vim.loop
+
 local M = {}
 
--- Utility function to get the Python executable from the virtual environment
-local function get_python_executable()
-    local venv_path = os.getenv('VIRTUAL_ENV') or (vim.fn.getcwd() .. '/.venv')
-    if vim.fn.executable(venv_path .. '/bin/python') == 1 then
-        return venv_path .. '/bin/python'
+-- Function to create a temporary file
+local function create_temp_file(content)
+    local tmpname = vim.fn.tempname()
+    local fd = assert(uv.fs_open(tmpname, "w", 384)) -- 384 is octal for 0600 permissions
+    uv.fs_write(fd, content, -1)
+    uv.fs_close(fd)
+    return tmpname
+end
+
+-- Function to read the content of a file
+local function read_file(filepath)
+    local fd = assert(uv.fs_open(filepath, "r", 438)) -- 438 is octal for 0666 permissions
+    local stat = assert(uv.fs_fstat(fd))
+    local data = uv.fs_read(fd, stat.size, 0)
+    uv.fs_close(fd)
+    return data
+end
+
+-- Function to format using black and isort
+function M.format_code()
+    if vim.bo.filetype == 'python' then
+        local content = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, true), "\n")
+        local tmpfile = create_temp_file(content)
+        local black_cmd = string.format('black --line-length 79 %s', tmpfile)
+        local isort_cmd = string.format('isort %s', tmpfile)
+
+        -- Run the commands and capture output
+        local black_output = vim.fn.system(black_cmd)
+        local black_status = vim.v.shell_error
+        local isort_output = vim.fn.system(isort_cmd)
+        local isort_status = vim.v.shell_error
+
+        -- Read the formatted content back into the buffer
+        local formatted_content = read_file(tmpfile)
+        vim.api.nvim_buf_set_lines(0, 0, -1, true, vim.split(formatted_content, "\n"))
+
+        -- Display messages in the command line area
+        local messages = {}
+        if black_status == 0 then
+            table.insert(messages, "Formatted with black")
+        else
+            table.insert(messages, "Error running black: " .. black_output)
+        end
+
+        if isort_status == 0 then
+            table.insert(messages, "Sorted imports with isort")
+        else
+            table.insert(messages, "Error running isort: " .. isort_output)
+        end
+
+        for _, msg in ipairs(messages) do
+            if msg:match("^Error") then
+                vim.cmd('echohl ErrorMsg | echom "' .. msg:gsub('"', '\\"') .. '" | echohl None')
+            else
+                vim.cmd('echom "' .. msg:gsub('"', '\\"') .. '"')
+            end
+        end
+
+        -- Avoid pressing ENTER to continue
+        vim.cmd('redraw!')
+
+        -- Remove the temporary file
+        uv.fs_unlink(tmpfile)
     end
-    return 'python'
 end
 
--- Function to run shell commands and capture output
-local function run_command(command)
-    local handle = io.popen(command)
-    local result = handle:read("*a")
-    handle:close()
-    return result
+-- Function to check with flake8
+function M.check_with_flake8()
+    if vim.bo.filetype == 'python' then
+        local filepath = fn.expand('%:p')
+        local flake8_cmd = string.format('flake8 --max-line-length 79 %s', filepath)
+
+        -- Run the command and capture output
+        local flake8_output = vim.fn.system(flake8_cmd)
+        local flake8_status = vim.v.shell_error
+
+        -- Display flake8 output in the command line area
+        local messages = {}
+        if flake8_status ~= 0 then
+            table.insert(messages, "Flake8 found PEP8 issues:\n" .. flake8_output)
+        end
+
+        for _, msg in ipairs(messages) do
+            if msg:match("^Flake8 found") then
+                for _, line in ipairs(vim.split(msg, "\n")) do
+                    vim.cmd('echohl ErrorMsg | echom "' .. line:gsub('"', '\\"') .. '" | echohl None')
+                end
+            else
+                vim.cmd('echom "' .. msg:gsub('"', '\\"') .. '"')
+            end
+        end
+    end
 end
 
--- Format and Lint on Save
-function M.format_and_lint()
-    local python_executable = get_python_executable()
-    local current_file = vim.api.nvim_buf_get_name(0)
-    
-    -- Run black with line length 79
-    local black_command = string.format("%s -m black --line-length 79 %s", python_executable, current_file)
-    print(run_command(black_command))
-    
-    -- Run isort with line length 79
-    local isort_command = string.format("%s -m isort --line-length 79 %s", python_executable, current_file)
-    print(run_command(isort_command))
-    
-    -- Run flake8 with max line length 79
-    local flake8_command = string.format("%s -m flake8 --max-line-length 79 %s", python_executable, current_file)
-    print(run_command(flake8_command))
-end
-
--- Jedi-based code navigation
-local function jedi_command(command)
-    local python_executable = get_python_executable()
-    local current_line = vim.api.nvim_get_current_line()
-    local cursor = vim.api.nvim_win_get_cursor(0)
-    local row, col = cursor[1], cursor[2]
-    local filename = vim.api.nvim_buf_get_name(0)
-
-    local script = string.format([[
-    import jedi
-    script = jedi.Script(%q, %d, %d, %q)
-    result = script.%s()
-    for item in result:
-        print(item)
-    ]], current_line, row, col, filename, command)
-
-    local cmd = string.format("%s -c %q", python_executable, script)
-    local result = run_command(cmd)
-    print(result)
-end
-
-function M.go_to_definition()
-    jedi_command('goto_definitions')
-end
-
-function M.find_references()
-    jedi_command('get_references')
-end
-
-function M.go_to_implementation()
-    jedi_command('goto_assignments')
-end
-
-function M.show_documentation()
-    jedi_command('help')
-end
-
--- Keybindings for navigation
-function M.setup_keybindings()
-    vim.api.nvim_set_keymap('n', 'gd', ':lua require"minimalist_pythonist".go_to_definition()<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', 'gr', ':lua require"minimalist_pythonist".find_references()<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', 'gi', ':lua require"minimalist_pythonist".go_to_implementation()<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', 'K', ':lua require"minimalist_pythonist".show_documentation()<CR>', { noremap = true, silent = true })
-end
-
--- Autocmd to format on save
+-- Setup auto commands
 function M.setup_autocmd()
-    vim.cmd [[autocmd BufWritePre *.py lua require'minimalist_pythonist'.format_and_lint()]]
+    vim.cmd [[autocmd BufWritePre *.py lua require'user.plugins.minimalist_pythonist'.format_code()]]
+    vim.cmd [[autocmd BufWritePost *.py lua require'user.plugins.minimalist_pythonist'.check_with_flake8()]]
 end
 
 function M.setup()
-    M.setup_keybindings()
     M.setup_autocmd()
 end
 
